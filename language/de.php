7c7391c44eec4b931ca0b91358c8e0aa5ad86396
André Matthies <matthiez@gmail.com>
2022-03-23 12:39:57 +0100

    add german translations

---
+++ language/de.php
@@ -0,0 +1,13 @@
+<?php return [
+    'apiKey' => 'API-Schlüssel',
+    'description' => 'SMS-Versand via sms77',
+    'error' => 'Fehler',
+    'from' => 'Absenderkennung',
+    'general' => 'Allgemein',
+    'name' => 'sms77',
+    'send' => 'Senden',
+    'sendBulk' => 'SMS-Massenversand',
+    'sms' => 'SMS',
+    'success' => 'Erfolg',
+    'text' => 'Text',
+];
